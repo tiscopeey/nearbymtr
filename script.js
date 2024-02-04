354c236deb7f84c584850842e99b020418412369
tiscopeey <126366485+tiscopeey@users.noreply.github.com>
2024-02-04 23:16:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const url = "https://script.google.com/macros/s/AKfycbzo06TDJr1oBadb6q7f9TTssuKwrBNc8UpQKEGJDXKriMxG7X4-3OxdH3uWTJ3Dsyn-Qg/exec?q=mtr";
+const mtrStop = [{"name":"博覽館","line":"AEL","code":"AWE","lat":22.322093914,"long":113.942820613},{"name":"機場","line":"AEL","code":"AIR","lat":22.315781487,"long":113.936590398},{"name":"青衣","line":"AEL","code":"TSY","lat":22.359262944,"long":114.107167657},{"name":"九龍","line":"AEL","code":"KOW","lat":22.305004575,"long":114.161317283},{"name":"香港","line":"AEL","code":"HOK","lat":22.284748543,"long":114.158020303},{"name":"欣澳","line":"DRL","code":"SUN","lat":22.332119855,"long":114.02885475},{"name":"迪士尼","line":"DRL","code":"DIS","lat":22.315346072,"long":114.045139134},{"name":"羅湖","line":"EAL","code":"LOW","lat":22.529052802,"long":114.113420699},{"name":"上水","line":"EAL","code":"SHS","lat":22.501492524,"long":114.127785637},{"name":"粉嶺","line":"EAL","code":"FAN","lat":22.492067743,"long":114.138692516},{"name":"太和","line":"EAL","code":"TWO","lat":22.450757341,"long":114.161573137},{"name":"大埔墟","line":"EAL","code":"TAP","lat":22.444310195,"long":114.170482165},{"name":"大學","line":"EAL","code":"UNI","lat":22.414072881,"long":114.210071825},{"name":"火炭","line":"EAL","code":"FOT","lat":22.395824208,"long":114.19850137},{"name":"沙田","line":"EAL","code":"SHT","lat":22.382126051,"long":114.186914844},{"name":"大圍","line":"EAL","code":"TAW","lat":22.373159039,"long":114.178554153},{"name":"九龍塘","line":"EAL","code":"KOT","lat":22.337560596,"long":114.175330917},{"name":"旺角東","line":"EAL","code":"MKK","lat":22.322018914,"long":114.172593949},{"name":"紅磡","line":"EAL","code":"HUH","lat":22.302864976,"long":114.182105256},{"name":"會展","line":"EAL","code":"EXC","lat":22.28136075,"long":114.176243514},{"name":"金鐘","line":"EAL","code":"ADM","lat":22.278535085,"long":114.164773713},{"name":"落馬洲","line":"EAL","code":"LMC","lat":22.515034254,"long":114.065624243},{"name":"柴灣","line":"ISL","code":"CHW","lat":22.264582041,"long":114.237134601},{"name":"杏花邨","line":"ISL","code":"HFC","lat":22.276627963,"long":114.239662548},{"name":"筲箕灣","line":"ISL","code":"SKW","lat":22.279232579,"long":114.228951388},{"name":"西灣河","line":"ISL","code":"SWH","lat":22.282178698,"long":114.221820484},{"name":"太古","line":"ISL","code":"TAK","lat":22.285074153,"long":114.215196281},{"name":"鰂魚涌","line":"ISL","code":"QUB","lat":22.288367494,"long":114.210158469},{"name":"北角","line":"ISL","code":"NOP","lat":22.290940553,"long":114.20043366},{"name":"炮台山","line":"ISL","code":"FOH","lat":22.288088402,"long":114.193653443},{"name":"天后","line":"ISL","code":"TIH","lat":22.282238422,"long":114.191846698},{"name":"銅鑼灣","line":"ISL","code":"CAB","lat":22.280641822,"long":114.184496274},{"name":"灣仔","line":"ISL","code":"WAC","lat":22.277552161,"long":114.172634101},{"name":"金鐘","line":"ISL","code":"ADM","lat":22.278535085,"long":114.164773713},{"name":"中環","line":"ISL","code":"CEN","lat":22.2818118,"long":114.157218955},{"name":"上環","line":"ISL","code":"SHW","lat":22.286353535,"long":114.15226459},{"name":"西營盤","line":"ISL","code":"SYP","lat":22.285513649,"long":114.142698173},{"name":"香港大學","line":"ISL","code":"HKU","lat":22.283474567,"long":114.136693142},{"name":"堅尼地城","line":"ISL","code":"KET","lat":22.281211431,"long":114.128396511},{"name":"調景嶺","line":"KTL","code":"TIK","lat":22.303931203,"long":114.252252297},{"name":"油塘","line":"KTL","code":"YAT","lat":22.297850893,"long":114.237139165},{"name":"藍田","line":"KTL","code":"LAT","lat":22.306828896,"long":114.232736906},{"name":"觀塘","line":"KTL","code":"KWT","lat":22.312330573,"long":114.226469358},{"name":"牛頭角","line":"KTL","code":"NTK","lat":22.315547525,"long":114.219210631},{"name":"九龍灣","line":"KTL","code":"KOB","lat":22.323513771,"long":114.214068723},{"name":"彩虹","line":"KTL","code":"CHH","lat":22.334965626,"long":114.209043514},{"name":"鑽石山","line":"KTL","code":"DIH","lat":22.340822626,"long":114.200841986},{"name":"黃大仙","line":"KTL","code":"WTS","lat":22.341677647,"long":114.193872218},{"name":"樂富","line":"KTL","code":"LOF","lat":22.338219192,"long":114.187675996},{"name":"九龍塘","line":"KTL","code":"KOT","lat":22.337560596,"long":114.175330917},{"name":"石硤尾","line":"KTL","code":"SKM","lat":22.331459303,"long":114.1686753},{"name":"太子","line":"KTL","code":"PRE","lat":22.324157828,"long":114.168593357},{"name":"旺角","line":"KTL","code":"MOK","lat":22.319300588,"long":114.16935224},{"name":"油麻地","line":"KTL","code":"YMT","lat":22.312846808,"long":114.170402449},{"name":"何文田","line":"KTL","code":"HOM","lat":22.309412144,"long":114.182804282},{"name":"黃埔","line":"KTL","code":"WHA","lat":22.30481422,"long":114.190196414},{"name":"烏溪沙","line":"TML","code":"WKS","lat":22.429287812,"long":114.243791313},{"name":"馬鞍山","line":"TML","code":"MOS","lat":22.424885417,"long":114.231948712},{"name":"恆安","line":"TML","code":"HEO","lat":22.417500484,"long":114.225739338},{"name":"大水坑","line":"TML","code":"TSH","lat":22.408227078,"long":114.222647637},{"name":"石門","line":"TML","code":"SHM","lat":22.387993077,"long":114.208638149},{"name":"第一城","line":"TML","code":"CIO","lat":22.382575738,"long":114.203228256},{"name":"沙田圍","line":"TML","code":"STW","lat":22.377014071,"long":114.194954322},{"name":"車公廟","line":"TML","code":"CKT","lat":22.374748157,"long":114.186108399},{"name":"大圍","line":"TML","code":"TAW","lat":22.373159039,"long":114.178554153},{"name":"顯徑","line":"TML","code":"HIK","lat":22.363803297,"long":114.170815931},{"name":"鑽石山","line":"TML","code":"DIH","lat":22.340822626,"long":114.200841986},{"name":"啟德","line":"TML","code":"KAT","lat":22.330854745,"long":114.200376879},{"name":"宋皇臺","line":"TML","code":"SUW","lat":22.326154863,"long":114.19192988},{"name":"土瓜灣","line":"TML","code":"TKW","lat":22.317917619,"long":114.187619123},{"name":"何文田","line":"TML","code":"HOM","lat":22.309412144,"long":114.182804282},{"name":"紅磡","line":"TML","code":"HUH","lat":22.302864976,"long":114.182105256},{"name":"尖東","line":"TML","code":"ETS","lat":22.295306421,"long":114.174652067},{"name":"柯士甸","line":"TML","code":"AUS","lat":22.305230729,"long":114.166121228},{"name":"南昌","line":"TML","code":"NAC","lat":22.326225517,"long":114.153307125},{"name":"美孚","line":"TML","code":"MEF","lat":22.33838125,"long":114.138348246},{"name":"荃灣西","line":"TML","code":"TWW","lat":22.369953205,"long":114.108223001},{"name":"錦上路","line":"TML","code":"KSR","lat":22.434824626,"long":114.063415203},{"name":"元朗","line":"TML","code":"YUL","lat":22.446072302,"long":114.035165304},{"name":"朗屏","line":"TML","code":"LOP","lat":22.447644108,"long":114.025769452},{"name":"天水圍","line":"TML","code":"TIS","lat":22.446754667,"long":114.003368135},{"name":"兆康","line":"TML","code":"SIH","lat":22.411850982,"long":113.978897552},{"name":"屯門","line":"TML","code":"TUM","lat":22.395001903,"long":113.973046156},{"name":"東涌","line":"TCL","code":"TUC","lat":22.289130111,"long":113.941245283},{"name":"欣澳","line":"TCL","code":"SUN","lat":22.332119855,"long":114.02885475},{"name":"青衣","line":"TCL","code":"TSY","lat":22.359262944,"long":114.107167657},{"name":"茘景","line":"TCL","code":"LAK","lat":22.348597052,"long":114.126130714},{"name":"南昌","line":"TCL","code":"NAC","lat":22.326225517,"long":114.153307125},{"name":"奧運","line":"TCL","code":"OLY","lat":22.317791804,"long":114.160248226},{"name":"九龍","line":"TCL","code":"KOW","lat":22.305004575,"long":114.161317283},{"name":"香港","line":"TCL","code":"HOK","lat":22.284748543,"long":114.158020303},{"name":"寶琳","line":"TKL","code":"POA","lat":22.322251503,"long":114.258008186},{"name":"坑口","line":"TKL","code":"HAH","lat":22.31573717,"long":114.264313129},{"name":"將軍澳","line":"TKL","code":"TKO","lat":22.307440316,"long":114.260008585},{"name":"調景嶺","line":"TKL","code":"TIK","lat":22.303931203,"long":114.252252297},{"name":"油塘","line":"TKL","code":"YAT","lat":22.297850893,"long":114.237139165},{"name":"鰂魚涌","line":"TKL","code":"QUB","lat":22.288367494,"long":114.210158469},{"name":"北角","line":"TKL","code":"NOP","lat":22.290940553,"long":114.20043366},{"name":"康城","line":"TKL","code":"LHP","lat":22.296270598,"long":114.269578742},{"name":"荃灣","line":"TWL","code":"TSW","lat":22.373536665,"long":114.117994654},{"name":"大窩口","line":"TWL","code":"TWH","lat":22.370647098,"long":114.124727523},{"name":"葵興","line":"TWL","code":"KWH","lat":22.363182902,"long":114.131203241},{"name":"葵芳","line":"TWL","code":"KWF","lat":22.356526322,"long":114.127564708},{"name":"茘景","line":"TWL","code":"LAK","lat":22.348597052,"long":114.126130714},{"name":"美孚","line":"TWL","code":"MEF","lat":22.33838125,"long":114.138348246},{"name":"茘枝角","line":"TWL","code":"LCK","lat":22.33725099,"long":114.147956607},{"name":"長沙灣","line":"TWL","code":"CSW","lat":22.335545452,"long":114.156052667},{"name":"深水埗","line":"TWL","code":"SSP","lat":22.330886338,"long":114.162120084},{"name":"太子","line":"TWL","code":"PRE","lat":22.324157828,"long":114.168593357},{"name":"旺角","line":"TWL","code":"MOK","lat":22.319300588,"long":114.16935224},{"name":"油麻地","line":"TWL","code":"YMT","lat":22.312846808,"long":114.170402449},{"name":"佐敦","line":"TWL","code":"JOR","lat":22.304806561,"long":114.171653087},{"name":"尖沙咀","line":"TWL","code":"TST","lat":22.297699483,"long":114.17217739},{"name":"金鐘","line":"TWL","code":"ADM","lat":22.278535085,"long":114.164773713},{"name":"中環","line":"TWL","code":"CEN","lat":22.2818118,"long":114.157218955},{"name":"金鐘","line":"SIL","code":"ADM","lat":22.278535085,"long":114.164773713},{"name":"海洋公園","line":"SIL","code":"OCP","lat":22.248663125,"long":114.174381157},{"name":"黃竹坑","line":"SIL","code":"WCH","lat":22.248003669,"long":114.168065791},{"name":"利東","line":"SIL","code":"LET","lat":22.243640308,"long":114.1553471},{"name":"海怡半島","line":"SIL","code":"SOH","lat":22.24346701,"long":114.148928653}];
 const xhttpr = new XMLHttpRequest();
-let lat, lng, response, received = 0;
-xhttpr.open("GET", url, true);
-
-xhttpr.send();
-
-xhttpr.onload = ()=> {
-	if (xhttpr.status == 200){
-		response = JSON.parse(xhttpr.response);
-		//let x = "<tr><td style='width:14%;'><strong>路線</strong></td><td style='width:86%;'><strong>方向</strong></td></tr>";
-		received++
-		if (received == 2){
-			getClosestStop();
-		}
-	} else {
-		//idk do sth
-	}
-}
-
+let lat, lng, response = mtrStop;
 
 if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -25,23 +8,18 @@ if (navigator.geolocation) {
 	console.log("no");
 }
 
-
 function showPosition(position) {
 	lat = position.coords.latitude, lng = position.coords.longitude;
 	console.log(lat + ", " + lng);
 	let url = "https://script.google.com/macros/s/AKfycbzo06TDJr1oBadb6q7f9TTssuKwrBNc8UpQKEGJDXKriMxG7X4-3OxdH3uWTJ3Dsyn-Qg/exec?q=markdown&des=MTR-Info&lat=" + lat + "&lng=" + lng;
 	xhttpr.open("GET", url, true);
 	xhttpr.send();
-	console.log("maps.google.com/?q=" + lat + "," + lng);
-	received++
-	if (received == 2){
-		getClosestStop();
-	}  
+	getClosestStop();
 }
 
 function getClosestStop (){
 	let shortestDistance = getDistanceFromLatLonInKm(lat, lng, parseFloat(response[0]["lat"]), parseFloat(response[0]["long"])), distance, stop, stop_id, allLine = [], apiReceived = 0, x = "";
-	let y = "<div class='centerDiv'>"
+	let y = "<div class='centerDiv'>", note = "";
 	for (let i = 0; i < response.length; i++){
 		distance = getDistanceFromLatLonInKm(lat, lng, parseFloat(response[i]["lat"]), parseFloat(response[i]["long"]));
 		if (distance < shortestDistance){
@@ -54,7 +32,6 @@ function getClosestStop (){
 	for (let i = 0; i < response.length; i++){
 		if (stop == response[i]["name"]){
 			allLine.push({line: response[i]["line"], sta: response[i]["code"], name: response[i]["name"]});
-			
 		}
 	}
 	
@@ -69,36 +46,53 @@ function getClosestStop (){
 		xhttpr.onload = ()=> {
 			if (xhttpr.status == 200){
 				raw = JSON.parse(xhttpr.response);
-				list = raw["data"][lineStation];
 				apiReceived++;
-				x = x + "<table id='" + allLine[i]["line"] + "' class='timetable' style='display: none'><tr><td><strong>抵站時間</strong></td><td><strong>終點站</strong></td><td><strong>月台</strong></td></tr>";
-				if (list["UP"] != null){
-					for (let j = 0; j < list["UP"].length; j++){
-						etaTime = new Date(list["UP"][j]["time"]);
-						etaTime = etaTime.toLocaleTimeString('en-HK', {hourCycle: 'h23'});
-						chunk += "<tr><td>" + etaTime + "</td><td>" + response[response.map(e => e.code).indexOf(list["UP"][j]["dest"])]["name"] + "</td><td>" + list["UP"][j]["plat"] + "</td></tr>";
-						destination.push(response[response.map(e => e.code).indexOf(list["UP"][j]["dest"])]["name"]);
+				if (raw["status"] == 0){
+					x += "<table id='" + allLine[i]["line"] + "' class='timetable' style='display: none'><tr><td>Error: " + raw["message"] + "</td></tr></table>"
+				} else {
+					list = raw["data"][lineStation];
+					x = x + "<table id='" + allLine[i]["line"] + "' class='timetable' style='display: none'><tr><td><strong>抵站時間</strong></td><td><strong>終點站</strong></td><td><strong>月台</strong></td></tr>";
+					if (list["UP"] != null){
+						for (let j = 0; j < list["UP"].length; j++){
+							note = "";
+							if (allLine[i]["line"] == "EAL"){
+								if (list["UP"][j]["route"] == "RAC"){
+									note = "<span style='font-size: 75%'>經馬場</span>";
+								}
+							}
+							etaTime = new Date(list["UP"][j]["time"]);
+							etaTime = etaTime.toLocaleTimeString('en-HK', {hourCycle: 'h23'});
+							chunk += "<tr><td>" + etaTime + "</td><td>" + response[response.map(e => e.code).indexOf(list["UP"][j]["dest"])]["name"] + note + "</td><td>" + list["UP"][j]["plat"] + "</td></tr>";
+							destination.push(response[response.map(e => e.code).indexOf(list["UP"][j]["dest"])]["name"]);
+						}
+						destination = [...new Set(destination)];
+						x = x + "<tr><td colspan='3' style='background-color: #339933;'>往： " + destination.join("/") + "</td></tr>" + chunk;
 					}
-					destination = [...new Set(destination)];
-					x = x + "<tr><td colspan='3' style='background-color: #339933;'>往： " + destination.join("/") + "</td></tr>" + chunk;
-				}
-				chunk = "", destination = [];
-				if (list["DOWN"] != null){
-					for (let j = 0; j < list["DOWN"].length; j++){
-						etaTime = new Date(list["DOWN"][j]["time"]);
-						etaTime = etaTime.toLocaleTimeString('en-HK', {hourCycle: 'h23'});
-						chunk += "<tr><td>" + etaTime + "</td><td>" + response[response.map(e => e.code).indexOf(list["DOWN"][j]["dest"])]["name"] + "</td><td>" + list["DOWN"][j]["plat"] + "</td></tr>";
-						destination.push(response[response.map(e => e.code).indexOf(list["DOWN"][j]["dest"])]["name"]);
+					chunk = "", destination = [];
+					if (list["DOWN"] != null){
+						for (let j = 0; j < list["DOWN"].length; j++){
+							note = "";
+							if (allLine[i]["line"] == "EAL"){
+								if (list["UP"][j]["route"] == "RAC"){
+									note = "<span style='font-size: 75%'>經馬場</span>";
+								}
+							}
+							etaTime = new Date(list["DOWN"][j]["time"]);
+							etaTime = etaTime.toLocaleTimeString('en-HK', {hourCycle: 'h23'});
+							chunk += "<tr><td>" + etaTime + "</td><td>" + response[response.map(e => e.code).indexOf(list["DOWN"][j]["dest"])]["name"] + note + "</td><td>" + list["DOWN"][j]["plat"] + "</td></tr>";
+							destination.push(response[response.map(e => e.code).indexOf(list["DOWN"][j]["dest"])]["name"]);
+						}
+						destination = [...new Set(destination)];
+						x = x + "<tr><td colspan='3' style='background-color: #339933;'>往： " + destination.join("/") + "</td></tr>" + chunk;
 					}
-					destination = [...new Set(destination)];
-					x = x + "<tr><td colspan='3' style='background-color: #339933;'>往： " + destination.join("/") + "</td></tr>" + chunk;
+					x = x + "</table>";
 				}
-				x = x + "</table>";
-				y += "<button class='btnOne' onclick='changeTable(\"" + allLine[i]["line"] + "\")' style='border-radius: 5px'>" + mtrLineName(allLine[i]["line"]) + "</button>";
+				y += "<button class='btnOne' id='btn" + allLine[i]["line"] + "' onclick='changeTable(\"" + allLine[i]["line"] + "\")' style='border-radius: 5px'>" + mtrLineName(allLine[i]["line"]) + "</button>";
 				if (apiReceived == allLine.length){
 					document.getElementById("station").innerText = allLine[0]["name"] + "站";
-					document.getElementById("waiting").style.display = "none";
 					document.getElementById("etaList").innerHTML = y + "</div>" + x;
+					document.getElementById("btn" + allLine[0]["line"]).style["background-color"] = "#006B00";
+					document.getElementById("waiting").style.display = "none";
 					document.getElementById(allLine[0]["line"]).style.display = "";
 				}
 			} else {
@@ -114,7 +108,12 @@ function changeTable (line){
 	for (let i = 0; i < collection.length; i++){
 		collection[i].style.display = "none";
 	}
+	const btnCollection = document.getElementsByTagName("button");
+	for (let i = 0; i < btnCollection.length; i++){
+		btnCollection[i].style["background-color"] = "#0B0";
+	}
 	document.getElementById(line).style.display = "";
+	document.getElementById("btn" + line).style["background-color"] = "#006B00";
 }
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
